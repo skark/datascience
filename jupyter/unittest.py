e4bfe0036d2287bdefc76016e3004afdddcd42e3
skark <figle32af@hotmail.com>
2019-12-26 19:32:23 -0800

    Update unittest.py

--- jupyter/unittest.py
+++ jupyter/unittest.py
@@ -11,11 +11,22 @@ import pandas as pd
 print("Python Version: " + sys.version)
 print("Matplotlib version: " + matplot.__version__)
 #print("Matplotlib numpy version: " + matplot.__version__numpy__)
+
 print("Numpy version: " + np.__version__)
+numpy.show_config()
+
 print("Scikit version: " + sklearn.__version__)
-print("OpenCV version: " + cv2.__version__)
+sklearn.show_versions()
+
 print("Tensorflow version: " + tf.__version__)
+print("Tensor Flow MKL Enabled: " + str(tensorflow.pywrap_tensorflow.IsMklEnabled()))
+
+print("OpenCV version: " + cv2.__version__)
 print("Pytorch version:" + torch.__version__)
 print("Scipy version:" + scipy.version.full_version)
 print("Pandas version:" + pd.__version__)
 print(pd.show_versions(as_json=False))
+
+
+
+
