d03b350d3f66c1a1ef3634cc00988cb45291afd8
skark <figle32af@hotmail.com>
2021-03-26 20:51:30 -0700

    Update unittest.py

--- jupyter/unittest.py
+++ jupyter/unittest.py
@@ -2,7 +2,7 @@ import sys
 import matplotlib as matplot
 import numpy as np
 import sklearn
-import cv2
+#import cv2
 import tensorflow as tf
 import torch
 import scipy
@@ -19,7 +19,7 @@ print("Scikit version: " + sklearn.__version__)
 sklearn.show_versions()
 
 print("Tensorflow version: " + tf.__version__)
-print("OpenCV version: " + cv2.__version__)
+#print("OpenCV version: " + cv2.__version__)
 print("Pytorch version:" + torch.__version__)
 print("Scipy version:" + scipy.version.full_version)
 print("Pandas version:" + pd.__version__)
